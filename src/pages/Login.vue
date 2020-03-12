0ac20b4a22d8ae7f1beca1c91fbf5fab6d97a11a
Nicolas Lima <nicolas.pedroso2145@gmail.com>
2020-03-12 16:44:08 -0300

    Login and Signup pages created

---
+++ src/pages/Login.vue
@@ -0,0 +1,20 @@
+<template>
+  <q-page class="flex flex-center">
+    Login
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+
+  data() {
+    return {
+      user: false
+    }
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/pages/Signup.vue
@@ -0,0 +1,20 @@
+<template>
+  <q-page class="flex flex-center">
+    Signup
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'Signup',
+
+  data() {
+    return {
+      user: false
+    }
+  }
+}
+</script>
+
+<style>
+</style>
