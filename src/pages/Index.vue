ec90bde049620a90e77f9f86856287b77eb387ac
Nicolas Lima <nicolas.pedroso2145@gmail.com>
2020-03-12 16:05:02 -0300

    HeatMap Implemented

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,5 +1,9 @@
 <template>
   <q-page class="flex flex-center">
+    <div class="container" id="teste" name="teste">
+      dsasdsdadsasaddsa
+    </div>
+    <p id="lorem" name="lorem">oi</p>
     <img
       alt="Quasar logo"
       src="~assets/quasar-logo-full.svg"
@@ -9,6 +13,46 @@
 
 <script>
 export default {
-  name: 'PageIndex'
+  name: 'PageIndex',
+
+  data() {
+    return {
+      heatmap: false
+    }
+  },
+
+  mounted() {
+    var heatmapInstance = require('heatmap.js')
+
+    var element = document.getElementById('teste')
+
+    var heatmap = heatmapInstance.create({
+      "container": document.querySelector(".container"),
+      "radius" : 90,
+      gradient: {
+        '0.1': '#000000',
+        '0.5': 'orange',
+        '1.0': 'red'
+      },
+    })
+
+    document.querySelector('.container').onmousemove = function(ev) {
+      console.log('im here', ev)
+      heatmap.addData({
+        x: ev.layerX,
+        y: ev.layerY,
+        value: .6,
+      });
+    };
+
+    console.log(heatmap)
+  }
 }
 </script>
+
+<style>
+.container{
+  height: 30em;
+  width: 30em;
+}
+</style>
