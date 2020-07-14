b3db338b993593ead37fbdabc52b01ead6a96832
Nicolas Lima <nicolas.pedroso2145@gmail.com>
2020-07-13 21:13:18 -0300

    Fixing dependencies problems, connection with Arduino successfuly estabilished

--- src/handler/ArduinoHandler.js
+++ src/handler/ArduinoHandler.js
@@ -1,9 +1,15 @@
+import feetProps from './FeetDataHandler'
+
+var OS = require('os');
+
+var isWindows = OS.platform().includes('win32') || OS.platform().includes('win64')
+
 const ArduinoBoard = require('serialport');
 
 const Readline = ArduinoBoard.parsers.Readline;
 
 // Be careful with Windows Hosts
-const port = new ArduinoBoard('/dev/ttyACM0');
+const port = new ArduinoBoard((isWindows ? 'COM4' : '/dev/ttyACM0'));
 
 const parser = new Readline();
 
@@ -12,5 +18,12 @@ port.pipe(parser);
 parser.on('data', (sensors) => {
   console.log("Received: " + sensors);
   var ldrValue = sensors.split(':');
-  console.log("temperatura: " + ldrValue[0]);
+  console.log("Intensity: " + ldrValue[0]);
+
+  feetProps.push({
+    X: 0,
+    y: 0,
+    Intensity: ldrValue[0]
+  })
+  
 })
---
+++ src/handler/FeetDataHandler.js
@@ -0,0 +1,7 @@
+let feetProps = {
+  X: 0,
+  Y: 0,
+  Intensity: 0
+}
+
+export default feetProps
